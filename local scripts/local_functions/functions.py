f8ec6f2a110bb5380f3da0f14084db38a1f46494
NilRadle <khaibulin.98@mail.ru>
2024-12-09 11:35:43 +0200

    arrangement of repo

--- local scripts/local_functions/functions.py
+++ local scripts/local_functions/functions.py
@@ -1,7 +1,10 @@
+import pandas as pd
+
+from scipy.stats import spearmanr
 from Bio.Data.CodonTable import unambiguous_dna_by_id
 from Bio.SeqUtils import seq3
 
-def aa_dict_creator(letters_3: bool = True , CODON_TABLE = 2, full = True):
+def aa_dict_creator(letters_3: bool = True , CODON_TABLE: int = 2, full: bool = True) -> dict[str, list[str]]:
     aa_dict = {}
     stop_codons = {codon:'_' for codon in unambiguous_dna_by_id[CODON_TABLE].stop_codons}
     c_aa_dic = unambiguous_dna_by_id[CODON_TABLE].forward_table | stop_codons
@@ -21,3 +24,10 @@ def aa_dict_creator(letters_3: bool = True , CODON_TABLE = 2, full = True):
             aa_dict[aa] = []
         aa_dict[aa].append(codon)
     return dict(sorted(aa_dict.items()))
+
+def correlation_calculation(cu_records: pd.DataFrame) -> pd.Series:
+    observed_cu = cu_records.iloc[0]
+    stat_values: pd.Series = cu_records.apply(spearmanr, axis=1, b=observed_cu)
+    spear_corr = pd.DataFrame(data={'rho': stat_values.str[0],
+                                    'pvalue': stat_values.str[1]})
+    return spear_corr
